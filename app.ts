584d49c9a4ccd86399dd535ee3e3768892699c5a
Jacob Zuo <chopinsky@live.com>
2018-10-26 22:02:46 -0500

    adding ts entry points

---
+++ app.ts
@@ -0,0 +1,11 @@
+class main {
+  public static run(): number {
+    let x: number = 10;
+    let y: number = 32;
+
+    console.log("Hello World! The answer is: ", x + y);
+    return 0;
+  }
+}
+
+main.run();

51d2b26b8d8f2a4eae019268d9b6a209d724b641
Jacob Zuo <chopinsky@live.com>
2018-10-28 13:33:30 -0500

    adding more ts support

--- app.ts
+++ app.ts
@@ -1,5 +1,35 @@
+import { cases } from "./cases.js";
+
 class main {
   public static run(): number {
+    let prog: string = "";
+    let debugMode: boolean = false;
+
+    if (process.argv && process.argv.length > 2) {
+      let params = process.argv.slice(2);
+
+      for (let val of params) {
+        let command = val.toLowerCase();
+        if (command === "-d" || command === "--debug") {
+          debugMode = true;
+          continue;
+        }
+
+        if (!prog) {
+          for (let p of Object.keys(cases)) {
+            if (p.indexOf(command) === 0) {
+              prog = p;
+              break;
+            }
+          }
+        }
+      }
+    }
+
+    if (!prog) {
+      prog = "lru";
+    }
+
     let x: number = 10;
     let y: number = 32;
 

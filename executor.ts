6bccd7cbd628e827a5d829c72e387d3730a02166
Jacob Zuo <chopinsky@live.com>
2018-11-04 00:15:42 -0500

    more updates'

--- executor.ts
+++ executor.ts
@@ -6,14 +6,17 @@ export const Cases: [string] = ["3SumWithMulti"];
 
 export class Executor {
   static run(problem: string, debug: boolean) {
-    switch (problem) {
-      case "value":
-        const p = require(`${problem}`);
-        break;
+    const len: number = problem.length;
 
-      default:
-        console.error(`No matching case is founded for the case ${problem}...`);
-        break;
-    }
+    Cases.forEach(val => {
+      if (val.toLowerCase().substr(0, len) === problem) {
+        const p = require(`./Problems/${val}.ts`);
+        if (p) {
+          console.log(`Running problem: ${problem}...`);
+        }
+
+        return;
+      }
+    });
   }
 }

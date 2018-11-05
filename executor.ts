d6aa971ab8e728707348fed0e5b622dfde9ab7ac
Jacob Zuo <chopinsky@live.com>
2018-11-04 22:34:49 -0600

    more

--- executor.ts
+++ executor.ts
@@ -1,18 +1,22 @@
 export interface Problem {
-  runner: (testCase: any, debug: boolean) => void;
+  runner: (testCase: [any], debug: boolean) => void;
+  getTestCase: (caseNum: number) => [any];
 }
 
 export const Cases: [string] = ["3SumWithMulti"];
 
 export class Executor {
-  static run(problem: string, debug: boolean) {
+  static run(problem: string, caseNum: number, debug: boolean) {
     const len: number = problem.length;
 
     Cases.forEach(val => {
       if (val.toLowerCase().substr(0, len) === problem) {
-        const p = require(`./Problems/${val}.ts`);
+        const p: Problem = require(`./Problems/${val}.ts`);
         if (p) {
           console.log(`Running problem: ${problem}...`);
+
+          const testCase: [any] = p.getTestCase(caseNum);
+          p.runner(testCase, debug);
         }
 
         return;

537d102866d2943f976e103c158264464f8192b1
Jacob Zuo <chopinsky@live.com>
2018-11-07 22:07:47 -0600

    updating the map class

--- Utils/map.ts
+++ Utils/map.ts
@@ -1,1 +1,67 @@
-export class Map {}
+export interface Result<V> {
+  errMessage: string;
+  value: V;
+}
+
+export type KeyType = string | number | symbol;
+
+export class Map<V> {
+  constructor(readonly _store: object = {}) {}
+
+  add(key: KeyType, val: V): Result<V> {
+    return update(this._store, key, val, false);
+  }
+
+  insert(key: KeyType, val: V): Result<V> {
+    return update(this._store, key, val, true);
+  }
+
+  get(key: KeyType): Result<V> {
+    if (typeof key === "string" && key.length === 0) {
+      return resultFactory("Key cannot be empty");
+    }
+
+    if (!this._store.hasOwnProperty(key)) {
+      return resultFactory(
+        `This key: ${key.toString()} does not exist in the map`
+      );
+    }
+
+    return resultFactory(null, this._store[key]);
+  }
+}
+
+const update = <V extends {}>(
+  store: object,
+  key: KeyType,
+  val: V,
+  override: boolean
+): Result<V> => {
+  if (typeof key === "string" && key.length === 0) {
+    return resultFactory("Key cannot be empty");
+  }
+
+  let oldVal: V = undefined;
+  if (store.hasOwnProperty(key)) {
+    if (!override) {
+      return resultFactory(
+        "Key already exists; if you wish to update the value, use method `insert()` instead"
+      );
+    }
+
+    oldVal = store[key];
+  }
+
+  store[key] = val;
+  return resultFactory(null, oldVal);
+};
+
+const resultFactory = <V extends {}>(
+  errMessage: string | null,
+  value: V | undefined = undefined
+): Result<V> => {
+  return Object.freeze({
+    errMessage,
+    value
+  });
+};

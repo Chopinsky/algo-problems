b477ba02a17f5f35e7affa0ab354a5bc786e5c4d
Jacob Zuo <chopinsky@live.com>
2018-11-26 13:05:21 -0600

    finishing the problem

--- Problems/ShortestSuperstring.ts
+++ Problems/ShortestSuperstring.ts
@@ -1,29 +1,96 @@
 import { Problem, TestCase, TestCaseFactory } from "./Executor";
 
-export class ShortestSuperstring implements Problem {
-  private g: number[][];
-  private testCase: TestCase;
+interface BFSResult {
+  ary: number[];
+  score: number;
+}
 
-  solve(): void {}
+const BFSResultSpread = (result: BFSResult) => {
+  return {
+    ary: [...result.ary],
+    score: result.score
+  };
+};
+
+export class ShortestSuperstring implements Problem {
+  private debug: boolean;
+  private g: number[][];
+  private dataSet: string[];
+  private answer: string;
+
+  solve(): void {
+    let ans: BFSResult = {
+      ary: undefined,
+      score: Infinity
+    };
+
+    let len = this.dataSet.length;
+    for (let i = 0; i < len; i++) {
+      let temp = this.bfs(
+        i,
+        i,
+        {
+          ary: [],
+          score: 0
+        },
+        new Array(len)
+      );
+
+      if (this.debug) {
+        console.log(temp);
+      }
+
+      if (temp.score < ans.score) {
+        ans = temp;
+      }
+    }
+
+    if (this.debug) {
+      console.log(ans);
+    }
+
+    //todo: create the parsed string...
+    let result = this.dataSet[ans.ary[0]];
+    for (let i = 1; i < ans.ary.length; i++) {
+      let parentIndex = ans.ary[i - 1];
+      let index = ans.ary[i];
+      let word = this.dataSet[index];
+
+      result += word.substring(word.length - this.g[parentIndex][index]);
+    }
+
+    console.log(`
+      Answer:   ${result} (length: ${result.length})
+      Expected: ${this.answer} (length: ${this.answer.length})
+    `);
+  }
 
   make(caseNum: number, debug: boolean): void {
-    this.testCase = this.genTestCase(caseNum);
-    if (!this.testCase) {
+    let testCase = this.genTestCase(caseNum);
+    if (!testCase || !testCase.data) {
       return;
     }
 
-    let testData: string[] = this.testCase.data;
-    let len: number = testData.length;
+    this.dataSet = testCase.data;
+    this.answer = testCase.target;
+    this.debug = debug;
+
+    let len: number = this.dataSet.length;
     this.g = new Array(len);
 
     for (let i = 0; i < len; i++) {
       this.g[i] = new Array(len);
       for (let j = 0; j < len; j++) {
-        this.g[i][j] = this.calcOverlap(testData[i], testData[j]);
+        this.g[i][j] =
+          i === j
+            ? this.dataSet[i].length
+            : this.calcOverlap(this.dataSet[i], this.dataSet[j]);
       }
     }
 
-    console.log(this.g);
+    if (this.debug) {
+      console.log(this.g);
+    }
   }
 
   genTestCase(caseNum: number): TestCase {
@@ -34,6 +101,9 @@ export class ShortestSuperstring implements Problem {
           "alexlovesleetcode"
         );
 
+      case 2:
+        return TestCaseFactory(["ads", "bad", "dsg", "gg"], "badsgg");
+
       default:
         return TestCaseFactory(
           ["catg", "ctaagt", "gcta", "ttca", "atgcatc"],
@@ -42,17 +112,50 @@ export class ShortestSuperstring implements Problem {
     }
   }
 
-  calcOverlap(a: string, b: string): number {
+  private calcOverlap(a: string, b: string): number {
     if (!a || a.length === 0 || !b || b.length === 0) {
       return 0;
     }
 
-    let ans: number = 0;
-    for (let i = 0; i < a.length && i < b.length; i++) {
-      if (a.charAt(a.length - i - 1) === b.charAt(i)) {
-        ans = i + 1;
-      } else {
-        break;
+    let ans: number = b.length;
+    let head = a.length >= b.length ? a.length - b.length : 0;
+    let len = a.length - head;
+
+    for (let i = head; i < a.length; i++) {
+      if (a.substring(i) === b.substr(0, len)) {
+        return b.length - len;
+      }
+
+      len--;
+    }
+
+    return ans;
+  }
+
+  private bfs(
+    parent: number,
+    node: number,
+    result: BFSResult,
+    visited: boolean[]
+  ): BFSResult {
+    if (visited[node] === true) {
+      return result;
+    }
+
+    result.score += this.g[parent][node];
+    result.ary.push(node);
+    visited[node] = true;
+
+    let ans: BFSResult = result;
+    let minScore = Infinity;
+
+    for (let i = 0; i < this.dataSet.length; i++) {
+      if (!visited[i]) {
+        let temp = this.bfs(node, i, BFSResultSpread(result), [...visited]);
+        if (temp.score < minScore) {
+          ans = temp;
+          minScore = temp.score;
+        }
       }
     }
 

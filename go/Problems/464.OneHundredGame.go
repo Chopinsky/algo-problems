5d0433d34ba9e98684573ed4ad9f571dea74f672
Jacob Zuo <chopinsky@live.com>
2019-05-10 15:02:22 -0500

    new problem

---
+++ go/Problems/464.OneHundredGame.go
@@ -0,0 +1,26 @@
+package problems
+
+// XXX ...
+type XXX struct {
+	source []int
+	output int
+}
+
+// CreateXXX ...
+func CreateXXX() *XXX {
+	return &XXX{}
+}
+
+// Build ...
+func (p *XXX) Build(test int) {
+	switch test {
+	default:
+		p.source = nil
+		p.output = 0
+
+	}
+}
+
+// Run ...
+func (p *XXX) Run() {
+}

34b19a4dbf2d9edd73cca7c929c5e086ca9e9263
Jacob Zuo <chopinsky@live.com>
2019-09-22 17:22:24 -0500

    making small corrections and adding test cases

--- go/Problems/1000-1999/1191.KConcaMaxSum.go
+++ go/Problems/1000-1999/1191.KConcaMaxSum.go
@@ -22,7 +22,7 @@ func CreateKCMS() *KCMS {
 // Build ...
 func (p *KCMS) Build(test int) {
 	p.ResetGlobals()
-	p.testCount = 3
+	p.testCount = 6
 
 	switch test {
 	case 1:
@@ -34,6 +34,21 @@ func (p *KCMS) Build(test int) {
 		p.source = []int{-1, -2}
 		p.k = 7
 		p.output = 0
+
+	case 3:
+		p.source = []int{-5, 2, -1, 3, -4}
+		p.k = 4
+		p.output = 4
+
+	case 4:
+		p.source = []int{2, -5, 1, 1}
+		p.k = 11
+		p.output = 4
+
+	case 5:
+		p.source = []int{-2, 5, 1, -3}
+		p.k = 3
+		p.output = 8
 
 	default:
 		p.source = []int{1, 2}
@@ -50,7 +65,7 @@ func (p *KCMS) ResetGlobals() {
 // Run ...
 func (p *KCMS) Run() {
 	for j := 0; j < 10; j++ {
-		fmt.Println("============ Running Trial: ", j, " ============")
+		fmt.Println("========= Running Trial: ", j, " =========")
 
 		for i := 0; i < p.testCount; i++ {
 			p.Build(i)
@@ -67,29 +82,35 @@ func (p *KCMS) Run() {
 
 func calcKCMS(src []int, k int) int {
 	if k == 1 {
-		result, _ := calcSoleArr(src)
-		return result
+		max, _, _ := calcSingleArr(src, 1)
+		return max
 	}
 
 	// extend the src array to include the extension portion
 	size := len(src)
 	src = append(src, src...)
-	result, prefixSum := calcSoleArr(src)
+
+	// max1 is the global max, max2 is the original array max
+	max1, max2, prefixSum := calcSingleArr(src, size)
 
 	if d.DEBUG {
-		fmt.Println(result, prefixSum)
+		fmt.Println(max1, max2, prefixSum[size])
 	}
 
 	// if adding more repetition will get us larger results
-	if k > 2 && prefixSum[size] > 0 {
-		result += (k - 2) * prefixSum[size]
+	if k > 2 && prefixSum[size] > 0 && max1 != max2 {
+		max1 += (k - 2) * prefixSum[size]
 	}
 
-	return result
+	if max1 > max2 {
+		return max1
+	}
+
+	return max2
 }
 
-func calcSoleArr(src []int) (int, []int) {
-	result, size := 0, len(src)
+func calcSingleArr(src []int, originalSize int) (int, int, []int) {
+	max1, max2, size := 0, 0, len(src)
 
 	// extend the src array to include the extension portion
 	prefixSum := make([]int, size+1)
@@ -100,12 +121,22 @@ func calcSoleArr(src []int) (int, []int) {
 
 		if val < min {
 			min = val
-		} else if val-min > result {
-			result = val - min
+		} else {
+			diff := val - min
+
+			// the overall max
+			if diff > max1 {
+				max1 = diff
+			}
+
+			// the max from the sole array
+			if i < originalSize && diff > max2 {
+				max2 = diff
+			}
 		}
 
 		prefixSum[i+1] = val
 	}
 
-	return result, prefixSum
+	return max1, max2, prefixSum
 }

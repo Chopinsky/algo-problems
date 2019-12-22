2964f2f96489218ae3e929f00200c3de8dd8c9ed
Jacob Zuo <chopinsky@live.com>
2019-12-22 14:17:34 -0600

    new problem

--- go/Problems/1000-1999/1292.MaxSqrWidth.go
+++ go/Problems/1000-1999/1292.MaxSqrWidth.go
@@ -66,16 +66,18 @@ func CreateMSW() *MSW {
 			{1, 1, 3, 2, 4, 3, 2},
 			{1, 1, 3, 2, 4, 3, 2},
 		},
-		output: 2,
+		threshold: 4,
+		output:    2,
 	})
 
 	problems = append(problems, &MSWProblem{
 		source: [][]int{
-			{1, 1, 3, 2, 4, 3, 2},
-			{1, 1, 3, 2, 4, 3, 2},
-			{1, 1, 3, 2, 4, 3, 2},
+			{2, 2, 2, 2, 2},
+			{2, 2, 2, 2, 2},
+			{2, 2, 2, 2, 2},
 		},
-		output: 2,
+		threshold: 1,
+		output:    0,
 	})
 
 	return &MSW{
@@ -84,6 +86,165 @@ func CreateMSW() *MSW {
 	}
 }
 
+type square struct {
+	x   int
+	y   int
+	sum int
+}
+
 func (p *MSWProblem) calcMSW() int {
-	return -1
+	curr := p.source
+	h, w := len(p.source), len(p.source[0])
+
+	if h == 1 || w == 1 {
+		return 1
+	}
+
+	row := makeArr(h, w)
+	col := makeArr(h, w)
+	stack := []*square{}
+	valid := false
+
+	for i := 0; i < h; i++ {
+		for j := 0; j < w; j++ {
+			if j > 0 {
+				row[i][j] = row[i][j-1] + curr[i][j]
+			} else {
+				row[i][0] = curr[i][0]
+			}
+
+			if i > 0 {
+				col[i][j] = col[i-1][j] + curr[i][j]
+			} else {
+				col[0][j] = curr[0][j]
+			}
+
+			valid = valid || (curr[i][j] <= p.threshold)
+
+			if curr[i][j] < p.threshold && i+1 < h && j+1 < w {
+				stack = append(stack, &square{
+					x:   i,
+					y:   j,
+					sum: curr[i][j],
+				})
+			}
+		}
+	}
+
+	if !valid {
+		return 0
+	}
+
+	i := 1
+	for len(stack) > 0 {
+		next := []*square{}
+		valid = false
+
+		for _, anchor := range stack {
+			x0, y0 := anchor.x, anchor.y
+			x1, y1 := x0+i, y0+i
+
+			anchor.sum +=
+				calcSum(row, x0, y0, x0, y1) + calcSum(col, x0, y0, x1, y0) - curr[x1][y1]
+
+			valid = valid || anchor.sum <= p.threshold
+
+			if anchor.sum < p.threshold && anchor.x+i+1 < h && anchor.y+i+1 < w {
+				next = append(next, anchor)
+			}
+		}
+
+		i++
+		stack = next
+	}
+
+	return i
+}
+
+func (p *MSWProblem) calcMSW1() int {
+	curr := p.source
+	h, w := len(curr), len(curr[0])
+	found := false
+
+	row := makeArr(h, w)
+	col := makeArr(h, w)
+
+	for i := 0; i < h; i++ {
+		for j := 0; j < w; j++ {
+			if j > 0 {
+				row[i][j] = row[i][j-1] + curr[i][j]
+			} else {
+				row[i][0] = curr[i][0]
+			}
+
+			if i > 0 {
+				col[i][j] = col[i-1][j] + curr[i][j]
+			} else {
+				col[0][j] = curr[0][j]
+			}
+
+			found = found || (curr[i][j] <= p.threshold)
+		}
+	}
+
+	// fmt.Println(row, col)
+
+	if !found {
+		return 0
+	}
+
+	i := 1
+	for i < h {
+		next := makeArr(h, w)
+		found = false
+
+		for j := 0; j < h-i; j++ {
+			for k := 0; k < w-i; k++ {
+				j0, k0 := j+i, k+i
+
+				// fmt.Println(i, j, k, j0, k0)
+
+				a := calcSum(row, j, k, j, k0)
+				b := calcSum(col, j, k, j0, k)
+
+				next[j][k] = curr[j][k] + a + b - p.source[j0][k0]
+
+				found = found || next[j][k] <= p.threshold
+			}
+		}
+
+		// fmt.Println(found, next, p.source)
+
+		if !found {
+			break
+		}
+
+		i++
+		curr = next
+	}
+
+	return i
+}
+
+func calcSum(src [][]int, i0, j0, i1, j1 int) int {
+	if i0 == 0 || j0 == 0 {
+		return src[i1][j1]
+	}
+
+	if i0 == i1 {
+		j0--
+	} else {
+		i0--
+	}
+
+	return src[i1][j1] - src[i0][j0]
+}
+
+func makeArr(h, w int) [][]int {
+	arr := make([][]int, h)
+	for i := 0; i < h; i++ {
+		arr[i] = make([]int, w)
+	}
+
+	return arr
 }

e11ce8b185b97ce103c0b4ed24b82bfef17f3870
Jacob Zuo <chopinsky@live.com>
2020-01-03 10:13:38 -0600

    new problem

--- go/Problems/1000-1999/1296.DivArrInSetsOfKConsecutiveNum.go
+++ go/Problems/1000-1999/1296.DivArrInSetsOfKConsecutiveNum.go
@@ -2,6 +2,7 @@ package problems
 
 import (
 	"fmt"
+	"sort"
 
 	d "../../Utils"
 )
@@ -45,14 +46,18 @@ func (p *DASKCN) Run() {
 	}
 }
 
+var numCount map[int]int
+
 // ResetGlobals ...
 func (p *DASKCN) ResetGlobals() {
+	numCount = make(map[int]int)
 }
 
 // DASKCNProblem ...
 type DASKCNProblem struct {
 	source []int
-	output int
+	k      int
+	output [][]int
 }
 
 // CreateDASKCN ...
@@ -60,8 +65,27 @@ func CreateDASKCN() *DASKCN {
 	problems := make([]*DASKCNProblem, 0)
 
 	problems = append(problems, &DASKCNProblem{
-		source: nil,
-		output: -1,
+		source: []int{1, 2, 3, 3, 4, 4, 5, 6},
+		k:      4,
+		output: [][]int{{1, 2, 3, 4}, {3, 4, 5, 6}},
+	})
+
+	problems = append(problems, &DASKCNProblem{
+		source: []int{3, 2, 1, 2, 3, 4, 3, 4, 5, 9, 10, 11},
+		k:      3,
+		output: [][]int{{1, 2, 3}, {2, 3, 4}, {3, 4, 5}, {9, 10, 11}},
+	})
+
+	problems = append(problems, &DASKCNProblem{
+		source: []int{3, 3, 2, 2, 1, 1},
+		k:      3,
+		output: [][]int{{1, 2, 3}, {1, 2, 3}},
+	})
+
+	problems = append(problems, &DASKCNProblem{
+		source: []int{1, 2, 3, 4},
+		k:      3,
+		output: nil,
 	})
 
 	return &DASKCN{
@@ -70,6 +94,50 @@ func CreateDASKCN() *DASKCN {
 	}
 }
 
-func (p *DASKCNProblem) calcDASKCN() int {
-	return -1
+func (p *DASKCNProblem) buildCount() []int {
+	numbers := make([]int, 0)
+
+	for _, val := range p.source {
+		if _, ok := numCount[val]; ok {
+			numCount[val]++
+		} else {
+			numbers = append(numbers, val)
+			numCount[val] = 1
+		}
+	}
+
+	sort.Ints(numbers)
+
+	return numbers
+}
+
+func (p *DASKCNProblem) calcDASKCN() bool {
+	numbers := p.buildCount()
+
+	var start int
+
+	for len(numbers) > 0 {
+		start, numbers = numbers[0], numbers[1:]
+		if numCount[start] == 0 {
+			continue
+		}
+
+		count := numCount[start]
+		numCount[start] = 0
+
+		for i := 1; i < p.k; i++ {
+			if total, ok := numCount[start+i]; !ok || total < count {
+				if d.DEBUG {
+					fmt.Println(numCount)
+					fmt.Println(start, start+i, count, ok)
+				}
+
+				return false
+			}
+
+			numCount[start+i] -= count
+		}
+	}
+
+	return true
 }

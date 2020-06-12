3b3ee398f79fbc3cc2a163e9915bccb56117314e
Jacob Zuo <zuo@iastate.edu>
2020-06-11 23:06:49 -0500

    adding red-black gree impl

---
+++ shared/rb_tree.go
@@ -0,0 +1,197 @@
+package shared
+
+import "fmt"
+
+// RBTreeNode ...
+type RBTreeNode struct {
+	red    bool
+	key    string
+	val    int
+	left   *RBTreeNode
+	right  *RBTreeNode
+	parent *RBTreeNode
+}
+
+// NewTreeRoot ...
+func NewTreeRoot() *RBTreeNode {
+	root := newNode("", 0, nil)
+	root.red = false
+
+	return root
+}
+
+// Insert ...
+func (r *RBTreeNode) Insert(key string, val int) {
+	if key == "" {
+		return
+	}
+
+	if r.key == "" {
+		r.key = key
+		r.val = val
+		return
+	}
+
+	r.insert(key, val)
+}
+
+// Find ...
+func (r *RBTreeNode) Find(key string) (int, error) {
+	if r.key == key {
+		return r.val, nil
+	}
+
+	if key < r.key && r.left != nil {
+		return r.left.Find(key)
+	}
+
+	if key > r.key && r.right != nil {
+		return r.right.Find(key)
+	}
+
+	return 0, fmt.Errorf("the key: %s is not found", key)
+}
+
+// Delete ...
+func (r *RBTreeNode) Delete(key string) (int, error) {
+	return r.findDel(key, nil)
+}
+
+func newNode(key string, val int, parent *RBTreeNode) *RBTreeNode {
+	return &RBTreeNode{
+		red:    true,
+		key:    key,
+		val:    val,
+		left:   nil,
+		right:  nil,
+		parent: parent,
+	}
+}
+
+func (r *RBTreeNode) insert(key string, val int) {
+	// equal value, we're done.
+	if r.key == key {
+		r.val = val
+		return
+	}
+
+	var temp, child *RBTreeNode
+
+	if key < r.key {
+		if r.left != nil {
+			if key <= r.left.key {
+				r.left.insert(key, val)
+				return
+			}
+
+			// detach the subtree / leaf
+			temp = r.left
+			temp.parent = nil
+		}
+
+		child = newNode(key, val, r)
+
+		// insert the node
+		r.left = child
+		child.left = temp
+	} else {
+		if r.right != nil {
+			if key >= r.right.key {
+				r.right.insert(key, val)
+				return
+			}
+
+			// detach the subtree / leaf
+			temp = r.right
+			temp.parent = nil
+		}
+
+		child = newNode(key, val, r)
+
+		// insert the node
+		r.right = child
+		child.right = temp
+	}
+
+	// update the parent to fully reattach
+	if temp != nil {
+		temp.parent = child
+	}
+
+	child.balance()
+}
+
+func (r *RBTreeNode) balance() {
+	parent := r.parent
+	if parent == nil {
+		// r is root, and root is always black
+		r.red = false
+		return
+	}
+
+	if !parent.red {
+		// parent is black, r is red, we're done
+		return
+	}
+
+	gparent := parent.parent
+	if gparent == nil {
+		// parent is root, set its color to black (root is always black)
+		parent.red = false
+		return
+	}
+
+	var uncle *RBTreeNode
+	if gparent.left == parent {
+		uncle = gparent.right
+	} else {
+		uncle = gparent.left
+	}
+
+	// case 1
+	if uncle.red {
+		// flip parent and uncle, set gparent to red
+		parent.red = false
+		uncle.red = false
+		gparent.red = true
+
+		// now balance gparent
+		gparent.balance()
+		return
+	}
+
+	// case 2: uncle is black, left-left
+
+	// case 3: uncle is black, left-right
+
+	// case 4: uncle is black, right-left
+
+	// case 5: uncle is black, right-right
+}
+
+func (r *RBTreeNode) findDel(key string, p *RBTreeNode) (int, error) {
+	if r.key == key {
+		r.delBalance(p)
+		return r.val, nil
+	}
+
+	if key < r.key && r.left != nil {
+		return r.left.findDel(key, r)
+	}
+
+	if key > r.key && r.right != nil {
+		return r.right.findDel(key, r)
+	}
+
+	return 0, fmt.Errorf("the key: %s is not found", key)
+}
+
+func (r *RBTreeNode) delBalance(p *RBTreeNode) {
+
+}
+
+// Iterate ...
+func (r *RBTreeNode) Iterate() map[int][]*RBTreeNode {
+	store := make(map[int][]*RBTreeNode)
+	return store
+}

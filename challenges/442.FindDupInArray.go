e0519ecdf0f5ba5473afd98e7f82cfbcfae0da18
Chopinsky <chopinsky@live.com>
2020-08-06 11:05:29 -0500

    challege problem

---
+++ challenges/442.FindDupInArray.go
@@ -0,0 +1,42 @@
+package challenge
+
+func findDuplicates(nums []int) []int {
+  res := make([]int, 0, len(nums))
+
+  for i := range nums {
+    idx := abs(nums[i]) - 1
+    if nums[idx] < 0 {
+      res = append(res, idx+1)
+    } else {
+      nums[idx] = -1 * nums[idx]
+    }
+  }
+
+  // fmt.Println(nums)
+
+  return res
+}
+
+func abs(num int) int {
+  if num < 0 {
+    return -1 * num
+  }
+
+  return num
+}
+
+func findDuplicates1(nums []int) []int {
+  marker := make([]int, len(nums))
+  res := make([]int, 0, len(nums))
+
+  for i := range nums {
+    if marker[nums[i] - 1] > 0 {
+      res = append(res, nums[i])
+      continue
+    }
+
+    marker[nums[i] - 1]++
+  }
+
+  return res
+}

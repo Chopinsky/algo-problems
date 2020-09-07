ff7883e407ccffca53283f24f84b9c8ccf937898
Chopinsky <chopinsky@live.com>
2020-09-07 15:12:23 -0500

    new problem

---
+++ challenges/290.WordPattern.go
@@ -0,0 +1,34 @@
+package challenges
+
+func wordPattern(pattern string, str string) bool {
+	if str == "" || pattern == "" {
+		return false
+	}
+
+	s := strings.Split(str, " ")
+	if len(pattern) != len(s) {
+		return false
+	}
+
+	pmap := make(map[byte]string)
+	rmap := make(map[string]bool)
+
+	for i, char := range pattern {
+		// fmt.Println(char, s[i])
+
+		if word, ok := pmap[byte(char)]; ok {
+			if word != s[i] {
+				return false
+			}
+		} else {
+			if rmap[s[i]] {
+				return false
+			}
+
+			pmap[byte(char)] = s[i]
+			rmap[s[i]] = true
+		}
+	}
+
+	return true
+}

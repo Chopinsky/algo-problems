1414a59ca573e47a95d03468816dd39bb7f1cfb6
Chopinsky <chopinsky@live.com>
2021-01-18 15:28:50 -0600

    problem

---
+++ challenges/1999/1798.MaxNumKSumPairs.go
@@ -0,0 +1,59 @@
+package challenges
+
+/**
+You are given an integer array nums and an integer k.
+
+In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
+
+Return the maximum number of operations you can perform on the array.
+
+Example 1:
+
+Input: nums = [1,2,3,4], k = 5
+Output: 2
+Explanation: Starting with nums = [1,2,3,4]:
+- Remove numbers 1 and 4, then nums = [2,3]
+- Remove numbers 2 and 3, then nums = []
+There are no more pairs that sum up to 5, hence a total of 2 operations.
+
+Example 2:
+
+Input: nums = [3,1,3,4,3], k = 6
+Output: 1
+Explanation: Starting with nums = [3,1,3,4,3]:
+- Remove the first two 3's, then nums = [1,4,3]
+There are no more pairs that sum up to 6, hence a total of 1 operation.
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+1 <= k <= 109
+*/
+
+func maxOperations(nums []int, k int) int {
+	var ans int
+
+	counts := make(map[int]int)
+	for _, num := range nums {
+		counts[num]++
+	}
+
+	for val, cnt := range counts {
+		if cnt <= 0 || val > k {
+			continue
+		}
+
+		if 2*val == k {
+			ans += cnt / 2
+		} else {
+			d := min(cnt, counts[k-val])
+			ans += d
+			delete(counts, k-val)
+		}
+
+		delete(counts, val)
+	}
+
+	return ans
+}

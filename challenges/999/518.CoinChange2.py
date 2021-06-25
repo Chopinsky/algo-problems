c3a0a8d32a864f99ef5f370972b01645544aba54
Chopinsky <chopinsky@live.com>
2021-06-25 10:36:35 -0500

    np

---
+++ challenges/999/518.CoinChange2.py
@@ -0,0 +1,72 @@
+'''
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the number of combinations that make up that amount. If that amount of money cannot be made up by any combination of the coins, return 0.
+
+You may assume that you have an infinite number of each kind of coin.
+
+The answer is guaranteed to fit into a signed 32-bit integer.
+
+Example 1:
+
+Input: amount = 5, coins = [1,2,5]
+Output: 4
+Explanation: there are four ways to make up the amount:
+5=5
+5=2+2+1
+5=2+1+1+1
+5=1+1+1+1+1
+
+Example 2:
+
+Input: amount = 3, coins = [2]
+Output: 0
+Explanation: the amount of 3 cannot be made up just with coins of 2.
+
+Example 3:
+
+Input: amount = 10, coins = [10]
+Output: 1
+
+Constraints:
+
+1 <= coins.length <= 300
+1 <= coins[i] <= 5000
+All the values of coins are unique.
+0 <= amount <= 5000
+'''
+
+from typing import List
+
+class Solution:
+  def change(self, amount: int, coins: List[int]) -> int:
+    if amount == 0:
+      return 1
+
+    n = len(coins)
+    dp = [[0] * (amount+1) for _ in range(n)]
+    presums = [0] * (amount+1)
+
+    for i in range(n):
+      coin = coins[i]
+
+      for j in range(max(1, j), amount+1):
+        if j == coin:
+          dp[i][j] += 1
+
+        if j > coin and dp[i][j-coin] > 0:
+          dp[i][j] += dp[i][j-coin]
+
+        if i > 0 and j > coin:
+          dp[i][j] += presums[j-coin]
+
+      for j in range(1, amount+1):
+        presums[j] += dp[i][j]
+
+    # print(dp)
+
+    counts = 0
+    for i in range(n):
+      counts += dp[i][amount]
+
+    return counts
---
+++ challenges/999/684.RedundantConnection.py
@@ -0,0 +1,66 @@
+'''
+In this problem, a tree is an undirected graph that is connected and has no cycles.
+
+You are given a graph that started as a tree with n nodes labeled from 1 to n, with one additional edge added. The added edge has two different vertices chosen from 1 to n, and was not an edge that already existed. The graph is represented as an array edges of length n where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the graph.
+
+Return an edge that can be removed so that the resulting graph is a tree of n nodes. If there are multiple answers, return the answer that occurs last in the input.
+
+Example 1:
+
+Input: edges = [[1,2],[1,3],[2,3]]
+Output: [2,3]
+
+Example 2:
+
+Input: edges = [[1,2],[2,3],[3,4],[1,4],[1,5]]
+Output: [1,4]
+ 
+Constraints:
+
+n == edges.length
+3 <= n <= 1000
+edges[i].length == 2
+1 <= ai < bi <= edges.length
+ai != bi
+There are no repeated edges.
+The given graph is connected.
+'''
+
+
+from typing import List
+
+
+class Solution:
+  def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+    n = len(edges)
+    node = [i for i in range(n)]
+    
+    def union(i: int, j: int):
+      bi, bj = find(i), find(j)
+      
+      if bi <= bj:
+        node[bj] = bi
+      else:
+        node[bi] = bj
+      
+      return
+    
+    def find(i: int):
+      while node[i] != i:
+        i = node[i]
+        
+      return i
+    
+    for [a, b] in edges:
+      ra, rb = find(a-1), find(b-1)
+      if ra == rb:
+        return [a, b]
+      
+      # union(a-1, b-1)
+      if ra <= rb:
+        node[rb] = ra
+      else:
+        node[ra] = rb
+              
+    return edges[-1]
+    

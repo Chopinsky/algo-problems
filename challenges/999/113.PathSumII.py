eaec58bb63854571c9df2c5aaa0e131ae45c1bc7
Chopinsky <chopinsky@live.com>
2021-08-04 10:04:32 -0500

    np

---
+++ challenges/999/113.PathSumII.py
@@ -0,0 +1,65 @@
+# Definition for a binary tree node.
+'''
+Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where each path's sum equals targetSum.
+
+A leaf is a node with no children.
+
+Example 1:
+
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: [[5,4,11,2],[5,8,4,5]]
+
+Example 2:
+
+
+Input: root = [1,2,3], targetSum = 5
+Output: []
+
+Example 3:
+
+Input: root = [1,2], targetSum = 0
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-1000 <= Node.val <= 1000
+-1000 <= targetSum <= 1000
+'''
+
+
+from typing import List
+
+
+class TreeNode:
+  def __init__(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+class Solution:
+  def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
+    path = []
+    ans = []
+    
+    def iterate(root: TreeNode, target: int, running: int):
+      if not root:
+        return
+      
+      curr = running + root.val
+      path.append(root.val)
+      
+      if not root.left and not root.right:
+        if curr == target:
+          ans.append(path.copy())
+      else:
+        iterate(root.left, target, curr)
+        iterate(root.right, target, curr)
+        
+      path.pop()
+      
+    iterate(root, targetSum, 0)
+    
+    return ans

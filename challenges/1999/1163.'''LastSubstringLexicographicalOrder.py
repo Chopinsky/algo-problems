174e8e3ea932802d328a31a9cc7786c6f00e0728
iastate-zuo <62153389+iastate-zuo@users.noreply.github.com>
2021-08-20 14:40:54 -0500

    Update 1163.'''LastSubstringLexicographicalOrder.py
    adding problem description

--- challenges/1999/1163.'''LastSubstringLexicographicalOrder.py
+++ challenges/1999/1163.'''LastSubstringLexicographicalOrder.py
@@ -1,3 +1,23 @@
+'''
+Given a string s, return the last substring of s in lexicographical order.
+
+Example 1:
+
+Input: s = "abab"
+Output: "bab"
+Explanation: The substrings are ["a", "ab", "aba", "abab", "b", "ba", "bab"]. The lexicographically maximum substring is "bab".
+
+Example 2:
+
+Input: s = "leetcode"
+Output: "tcode"
+
+Constraints:
+
+1 <= s.length <= 4 * 105
+s contains only lowercase English letters.
+'''
+
 class Solution:
   def lastSubstring(self, s: str) -> str:
     n = len(s)

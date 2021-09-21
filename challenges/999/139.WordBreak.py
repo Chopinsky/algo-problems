bdd8730b1c88cf87c45f2d7663ba04b824709282
Chopinsky <chopinsky@live.com>
2021-09-21 09:19:20 -0500

    np

---
+++ challenges/999/139.WordBreak.py
@@ -0,0 +1,55 @@
+'''
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+Example 2:
+
+Input: s = "applepenapple", wordDict = ["apple","pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: false
+
+Constraints:
+
+1 <= s.length <= 300
+1 <= wordDict.length <= 1000
+1 <= wordDict[i].length <= 20
+s and wordDict[i] consist of only lowercase English letters.
+All the strings of wordDict are unique.
+'''
+
+from functools import lru_cache
+from typing import List
+
+class Solution:
+  def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+    n = len(s)
+    
+    @lru_cache(None)
+    def dp(i: int) -> bool:
+      nonlocal n
+      if i == n:
+        return True
+      
+      for w in wordDict:
+        ln = len(w)
+        if i + ln > n:
+          continue
+          
+        if s[i:i+ln] == w and dp(i+ln):
+          return True
+      
+    return dp(0)
+  

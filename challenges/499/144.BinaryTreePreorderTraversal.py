954c7153a93a842130a36595266e40f3420f4974
iastate-zuo <62153389+iastate-zuo@users.noreply.github.com>
2021-10-13 09:47:06 -0500

    Create 144.BinaryTreePreorderTraversal.py

---
+++ challenges/499/144.BinaryTreePreorderTraversal.py
@@ -0,0 +1,65 @@
+'''
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+Example 1:
+
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+
+Example 2:
+
+Input: root = []
+Output: []
+
+Example 3:
+
+Input: root = [1]
+Output: [1]
+
+Example 4:
+
+Input: root = [1,2]
+Output: [1,2]
+
+Example 5:
+
+Input: root = [1,null,2]
+Output: [1,2]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+'''
+
+
+# Definition for a binary tree node.
+class TreeNode:
+  def __init__(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+    
+    
+class Solution:
+  def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    ans = []
+    stack = [(root, 2)]
+    
+    while stack:
+      node, state = stack.pop()
+      if not node or not state:
+        continue
+      
+      if state == 2:
+        ans.append(node.val)
+        stack.append((node, 1))
+        stack.append((node.left, 2))
+        continue
+        
+      if state == 1:
+        stack.append((node.right, 2))
+        
+    return ans
+  

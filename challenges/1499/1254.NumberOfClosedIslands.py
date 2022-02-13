b93e27a2790a43c7c66e686a83a55a290dd35d1a
Chopinsky <chopinsky@live.com>
2022-02-13 11:15:01 -0600

    np

---
+++ challenges/1499/1254.NumberOfClosedIslands.py
@@ -0,0 +1,76 @@
+'''
+Given a 2D grid consists of 0s (land) and 1s (water).  An island is a maximal 4-directionally connected group of 0s and a closed island is an island totally (all left, top, right, bottom) surrounded by 1s.
+
+Return the number of closed islands.
+
+Example 1:
+
+Input: grid = [[1,1,1,1,1,1,1,0],[1,0,0,0,0,1,1,0],[1,0,1,0,1,1,1,0],[1,0,0,0,0,1,0,1],[1,1,1,1,1,1,1,0]]
+Output: 2
+Explanation: 
+Islands in gray are closed because they are completely surrounded by water (group of 1s).
+Example 2:
+
+
+Input: grid = [[0,0,1,0,0],[0,1,0,1,0],[0,1,1,1,0]]
+Output: 1
+Example 3:
+
+Input: grid = [[1,1,1,1,1,1,1],
+               [1,0,0,0,0,0,1],
+               [1,0,1,1,1,0,1],
+               [1,0,1,0,1,0,1],
+               [1,0,1,1,1,0,1],
+               [1,0,0,0,0,0,1],
+               [1,1,1,1,1,1,1]]
+Output: 2
+ 
+
+Constraints:
+
+1 <= grid.length, grid[0].length <= 100
+0 <= grid[i][j] <=1
+'''
+
+
+from typing import List, Tuple
+from collections import deque
+
+
+class Solution:
+  def closedIsland(self, grid: List[List[int]]) -> int:
+    seen = set()
+    m, n = len(grid), len(grid[0])
+    if m < 3 or n < 3:
+      return 0
+    
+    islands = {}
+    
+    for i in range(m):
+      for j in range(n):
+        if (i, j) in seen or grid[i][j] == 1:
+          continue
+          
+        stack = deque([(i, j)])
+        root = i*n + j
+        islands[root] = True
+        seen.add((i, j))
+        
+        while stack:
+          x, y = stack.popleft()
+          for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            x0, y0 = x+dx, y+dy
+            if x0 < 0 or x0 >= m or y0 < 0 or y0 >= n:
+              islands[root] = False
+              continue
+              
+            if grid[x0][y0] == 1 or (x0, y0) in seen:
+              continue
+              
+            stack.append((x0, y0))
+            seen.add((x0, y0))
+          
+    # print(islands)
+        
+    return sum(1 if islands[k] else 0 for k in islands)
+      
---
+++ challenges/2499/2044.CountNumMaxBitwiseORSubsets.py
@@ -0,0 +1,68 @@
+'''
+Given an integer array nums, find the maximum possible bitwise OR of a subset of nums and return the number of different non-empty subsets with the maximum bitwise OR.
+
+An array a is a subset of an array b if a can be obtained from b by deleting some (possibly zero) elements of b. Two subsets are considered different if the indices of the elements chosen are different.
+
+The bitwise OR of an array a is equal to a[0] OR a[1] OR ... OR a[a.length - 1] (0-indexed).
+
+Example 1:
+
+Input: nums = [3,1]
+Output: 2
+Explanation: The maximum possible bitwise OR of a subset is 3. There are 2 subsets with a bitwise OR of 3:
+- [3]
+- [3,1]
+
+Example 2:
+
+Input: nums = [2,2,2]
+Output: 7
+Explanation: All non-empty subsets of [2,2,2] have a bitwise OR of 2. There are 23 - 1 = 7 total subsets.
+
+Example 3:
+
+Input: nums = [3,2,1,5]
+Output: 6
+Explanation: The maximum possible bitwise OR of a subset is 7. There are 6 subsets with a bitwise OR of 7:
+- [3,5]
+- [3,1,5]
+- [3,2,5]
+- [3,2,1,5]
+- [2,5]
+- [2,1,5]
+ 
+
+Constraints:
+
+1 <= nums.length <= 16
+1 <= nums[i] <= 10^5
+'''
+
+
+from typing import List
+from collections import defaultdict, Counter
+
+
+class Solution:
+  def countMaxOrSubsets(self, nums: List[int]) -> int:
+    counter, nxt = defaultdict(int), defaultdict(int)
+    c = Counter(nums)
+    
+    for val in c:
+      base = (1 << c[val]) - 1
+      nxt[val] += base
+      # print(val, base, counter)
+      
+      # build all subsets with val
+      for vs, cnt in counter.items():
+        nxt[val|vs] += base * cnt
+        
+      # merge results
+      for vs, cnt in nxt.items():
+        counter[vs] += cnt
+        
+      nxt.clear()
+      
+    # print(counter)
+    return counter[max(counter)]
+    

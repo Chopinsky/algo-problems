13d20115d82046da5cc7bb6849bb9a32c1014e9c
Chopinsky <chopinsky@live.com>
2022-04-10 18:37:47 -0500

    np

---
+++ challenges/2499/2234_MaximumTotalBeautyGardens.py
@@ -0,0 +1,126 @@
+'''
+Alice is a caretaker of n gardens and she wants to plant flowers to maximize the total beauty of all her gardens.
+
+You are given a 0-indexed integer array flowers of size n, where flowers[i] is the number of flowers already planted in the ith garden. Flowers that are already planted cannot be removed. You are then given another integer newFlowers, which is the maximum number of flowers that Alice can additionally plant. You are also given the integers target, full, and partial.
+
+A garden is considered complete if it has at least target flowers. The total beauty of the gardens is then determined as the sum of the following:
+
+The number of complete gardens multiplied by full.
+The minimum number of flowers in any of the incomplete gardens multiplied by partial. If there are no incomplete gardens, then this value will be 0.
+Return the maximum total beauty that Alice can obtain after planting at most newFlowers flowers.
+
+ 
+
+Example 1:
+
+Input: flowers = [1,3,1,1], newFlowers = 7, target = 6, full = 12, partial = 1
+Output: 14
+Explanation: Alice can plant
+- 2 flowers in the 0th garden
+- 3 flowers in the 1st garden
+- 1 flower in the 2nd garden
+- 1 flower in the 3rd garden
+The gardens will then be [3,6,2,2]. She planted a total of 2 + 3 + 1 + 1 = 7 flowers.
+There is 1 garden that is complete.
+The minimum number of flowers in the incomplete gardens is 2.
+Thus, the total beauty is 1 * 12 + 2 * 1 = 12 + 2 = 14.
+No other way of planting flowers can obtain a total beauty higher than 14.
+Example 2:
+
+Input: flowers = [2,4,5,3], newFlowers = 10, target = 5, full = 2, partial = 6
+Output: 30
+Explanation: Alice can plant
+- 3 flowers in the 0th garden
+- 0 flowers in the 1st garden
+- 0 flowers in the 2nd garden
+- 2 flowers in the 3rd garden
+The gardens will then be [5,4,5,5]. She planted a total of 3 + 0 + 0 + 2 = 5 flowers.
+There are 3 gardens that are complete.
+The minimum number of flowers in the incomplete gardens is 4.
+Thus, the total beauty is 3 * 2 + 4 * 6 = 6 + 24 = 30.
+No other way of planting flowers can obtain a total beauty higher than 30.
+Note that Alice could make all the gardens complete but in this case, she would obtain a lower total beauty.
+ 
+
+Constraints:
+
+1 <= flowers.length <= 10^5
+1 <= flowers[i], target <= 10^5
+1 <= newFlowers <= 10^10
+1 <= full, partial <= 10^5
+'''
+
+from typing import List
+from bisect import bisect_left, bisect_right
+
+
+class Solution:
+  def maximumBeauty(self, flowers: List[int], newFlowers: int, target: int, full: int, partial: int) -> int:
+    n = len(flowers)
+    flowers.sort()
+    if flowers[0] >= target:
+      return n * full
+    
+    prefix = [val for val in flowers]
+    for i in range(1, n):
+      prefix[i] += prefix[i-1]
+      
+    idx = bisect_left(flowers, target)
+    if idx == 0:
+      return n * full
+      
+    rem = newFlowers
+    full_score = full * (n - idx)
+    max_score = full_score + flowers[0] * partial
+    # print('init', flowers, idx, full_score, max_score)
+    
+    def get_needed(m: int, i: int) -> int:
+      j = bisect_right(flowers, m) - 1
+      j = min(i, j)
+      if j < 0:
+        return 0
+      
+      if j >= n:
+        return m*n - prefix[-1]
+        
+      # print('needed:', m, i, j)
+      return max(0, m*(j+1) - prefix[j])
+    
+    def get_count(rem: int, i: int) -> int:
+      l, r = flowers[0], target
+      last = l
+      
+      while l < r:
+        m = (l + r) // 2
+        needed = get_needed(m, i)
+        
+        if needed == rem:
+          last = m
+          l = m
+          break
+          
+        if needed < rem:
+          last = m
+          l = m + 1
+        else:
+          r = m - 1
+          
+      # print(last, rem, l, get_needed(l, i))
+      if l < target and get_needed(l, i) <= rem:
+        last = max(last, l)
+        
+      return last
+    
+    while idx >= 0 and rem >= 0:
+      last = get_count(rem, idx-1) if idx > 0 else 0
+      # print('iter:', idx, last, full_score, last*partial + full_score, rem)
+      
+      max_score = max(max_score, last*partial + full_score)
+      idx -= 1    
+      
+      if idx >= 0:
+        full_score += full
+        rem -= target - flowers[idx]
+        
+    return max_score
+  
---
+++ challenges/499/373_FindKPairsWithSmallestSums.py
@@ -0,0 +1,66 @@
+'''
+You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k.
+
+Define a pair (u, v) which consists of one element from the first array and one element from the second array.
+
+Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
+
+Example 1:
+
+Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+Output: [[1,2],[1,4],[1,6]]
+Explanation: The first 3 pairs are returned from the sequence: [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+Example 2:
+
+Input: nums1 = [1,1,2], nums2 = [1,2,3], k = 2
+Output: [[1,1],[1,1]]
+Explanation: The first 2 pairs are returned from the sequence: [1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+Example 3:
+
+Input: nums1 = [1,2], nums2 = [3], k = 3
+Output: [[1,3],[2,3]]
+Explanation: All possible pairs are returned from the sequence: [1,3],[2,3]
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 10^5
+-10^9 <= nums1[i], nums2[i] <= 10^9
+nums1 and nums2 both are sorted in ascending order.
+1 <= k <= 10^4
+'''
+
+
+from typing import List
+from heapq import heappop, heappush
+
+
+class Solution:
+  def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+    m, n = len(nums1), len(nums2)
+    if m == 1:
+      return [[nums1[0], nums2[j]] for j in range(min(n, k))]
+    
+    if n == 1:
+      return [[nums1[i], nums2[0]] for i in range(min(m, k))]
+    
+    ans = []
+    heap = [(nums1[0]+nums2[0], 0, 0)]
+    seen = set([(0, 0)])
+    
+    while len(ans) < k and heap:
+      # print(ans, heap)
+      _, i, j = heappop(heap)
+      ans.append([nums1[i], nums2[j]])
+      
+      if len(ans)+len(heap) < 2*k:
+        if j < n-1 and (i, j+1) not in seen:
+          heappush(heap, (nums1[i]+nums2[j+1], i, j+1))
+          seen.add((i, j+1))
+          
+        if i < m-1 and (i+1, j) not in seen:
+          heappush(heap, (nums1[i+1]+nums2[j], i+1, j))
+          seen.add((i+1, j))
+      
+    return ans
+  

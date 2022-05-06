5d40579c67a8df3ed85e8a7ccf88eead57918a94
Chopinsky <chopinsky@live.com>
2022-05-06 10:34:24 -0500

    1717,1968,2233

---
+++ challenges/1999/1717_MaximumScoreFromRemovingSubstrings.py
@@ -0,0 +1,71 @@
+'''
+You are given a string s and two integers x and y. You can perform two types of operations any number of times.
+
+Remove substring "ab" and gain x points.
+For example, when removing "ab" from "cabxbae" it becomes "cxbae".
+Remove substring "ba" and gain y points.
+For example, when removing "ba" from "cabxbae" it becomes "cabxe".
+Return the maximum points you can gain after applying the above operations on s.
+
+Example 1:
+
+Input: s = "cdbcbbaaabab", x = 4, y = 5
+Output: 19
+Explanation:
+- Remove the "ba" underlined in "cdbcbbaaabab". Now, s = "cdbcbbaaab" and 5 points are added to the score.
+- Remove the "ab" underlined in "cdbcbbaaab". Now, s = "cdbcbbaa" and 4 points are added to the score.
+- Remove the "ba" underlined in "cdbcbbaa". Now, s = "cdbcba" and 5 points are added to the score.
+- Remove the "ba" underlined in "cdbcba". Now, s = "cdbc" and 5 points are added to the score.
+Total score = 5 + 4 + 5 + 5 = 19.
+Example 2:
+
+Input: s = "aabbaaxybbaabb", x = 5, y = 4
+Output: 20
+
+Constraints:
+
+1 <= s.length <= 10^5
+1 <= x, y <= 10^4
+s consists of lowercase English letters.
+'''
+
+
+class Solution:
+  def maximumGain(self, s: str, x: int, y: int) -> int:
+    stack = []
+    if x >= y:
+      a = ('a', 'b')
+      b = ('b', 'a')
+    else:
+      a = ('b', 'a')
+      b = ('a', 'b')
+      x, y = y, x
+      
+    score = 0
+    for ch in s:
+      if not stack:
+        stack.append(ch)
+        continue
+        
+      if ch == a[1] and stack[-1] == a[0]:
+        stack.pop()
+        score += x
+        continue
+        
+      stack.append(ch)
+        
+    nxt = []
+    for ch in stack:
+      if not nxt:
+        nxt.append(ch)
+        continue
+        
+      if ch == b[1] and nxt[-1]  == b[0]:
+        nxt.pop()
+        score += y
+        continue
+        
+      nxt.append(ch)
+      
+    return score
+  

1a4192eb82589a3b64e7ff2b5d920bd60539b47d
Chopinsky <chopinsky@live.com>
2022-06-25 08:38:26 -0500

    665,1387

--- challenges/999/665.NonDecreasingArray.py
+++ challenges/999/665.NonDecreasingArray.py
@@ -18,11 +18,39 @@ Explanation: You can't get a non-decreasing array by modify at most one element.
 Constraints:
 
 n == nums.length
-1 <= n <= 104
--105 <= nums[i] <= 105
+1 <= n <= 10^4
+-10^5 <= nums[i] <= 10^5
 '''
 
+from typing import List
+
+
 class Solution:
+  def checkPossibility(self, nums: List[int]) -> bool:
+    idx = -1
+    n = len(nums)
+    
+    for i in range(1, n):
+      if nums[i] < nums[i-1]:
+        if idx >= 0:
+          return False
+        
+        idx = i
+    
+    # print(stack)
+    if idx < 0:
+      return True
+    
+    def check(idx):
+      # print('check:', idx)
+      if n-1 > idx > 0 and nums[idx-1] > nums[idx+1]:
+        return False
+      
+      return True
+    
+    return check(idx) or check(idx-1)
+
+
   def checkPossibility(self, nums: List[int]) -> bool:
     pos = -1
     for i in range(len(nums)-1):

16d1dda15808895f7d31d2fa19ceb1b6f6fc46a7
Chopinsky <chopinsky@live.com>
2022-10-14 09:26:51 -0500

    2095,2432

--- challenges/2499/2095.DelMiddleNodeOfALinkedList.py
+++ challenges/2499/2095.DelMiddleNodeOfALinkedList.py
@@ -56,6 +56,24 @@ class Solution:
     if not head or not head.next:
       return None
     
+    if not head.next.next:
+      head.next = None
+      return head
+    
+    slow, fast, prev = head, head, None
+    while fast and fast.next:
+      prev = slow
+      slow = slow.next
+      fast = fast.next.next
+      
+    prev.next = slow.next
+    return head
+
+    
+  def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    if not head or not head.next:
+      return None
+    
     count = 0
     curr = head
     

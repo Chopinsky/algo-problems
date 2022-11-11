e3452aa8b6a5b8111182482bbad71292cb43d60d
Chopinsky <chopinsky@live.com>
2022-11-11 10:47:15 -0600

    26,2194

--- challenges/499/26.RemoveDuplicatesFromSortedArray.py
+++ challenges/499/26.RemoveDuplicatesFromSortedArray.py
@@ -49,6 +49,18 @@ import math
 
 class Solution:
   def removeDuplicates(self, nums: List[int]) -> int:
+    i, j = 1, 1
+    while j < len(nums):
+      if nums[j] != nums[i-1]:
+        nums[i] = nums[j]
+        i += 1
+        
+      j += 1
+      
+    return i
+    
+
+  def removeDuplicates(self, nums: List[int]) -> int:
     l = 0
     last = -math.inf
     

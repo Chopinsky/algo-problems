38566a07718e78801f44983946925822469b5849
Chopinsky <chopinsky@live.com>
2022-12-01 10:56:21 -0600

    592,2482

---
+++ challenges/999/592_Fraction_Addition_Subtraction.py
@@ -0,0 +1,78 @@
+'''
+592. Fraction Addition and Subtraction
+
+Given a string expression representing an expression of fraction addition and subtraction, return the calculation result in string format.
+
+The final result should be an irreducible fraction. If your final result is an integer, change it to the format of a fraction that has a denominator 1. So in this case, 2 should be converted to 2/1.
+
+Example 1:
+
+Input: expression = "-1/2+1/2"
+Output: "0/1"
+Example 2:
+
+Input: expression = "-1/2+1/2+1/3"
+Output: "1/3"
+Example 3:
+
+Input: expression = "1/3-1/2"
+Output: "-1/6"
+
+Constraints:
+
+The input string only contains '0' to '9', '/', '+' and '-'. So does the output.
+Each fraction (input and output) has the format ±numerator/denominator. If the first input fraction or the output is positive, then '+' will be omitted.
+The input only contains valid irreducible fractions, where the numerator and denominator of each fraction will always be in the range [1, 10]. If the denominator is 1, it means this fraction is actually an integer in a fraction format defined above.
+The number of given fractions will be in the range [1, 10].
+The numerator and denominator of the final result are guaranteed to be valid and in the range of 32-bit int.
+'''
+
+from math import gcd
+
+
+class Solution:
+  def fractionAddition(self, expr: str) -> str:
+    parts = []
+    n = len(expr)
+    last = 0
+    
+    for i in range(1, n):
+      if expr[i] == '+' or expr[i] == '-':
+        parts.append(expr[last:i])
+        last = i
+        
+    parts.append(expr[last:])
+    # print(parts)
+    
+    if len(parts) == 1:
+      return parts[0]
+    
+    def div(s: str):
+      res = s.split('/')
+      return int(res[0]), int(res[1])
+    
+    t0, b0 = div(parts[0])
+    # print('init:', t0, b0)
+    
+    for s in parts[1:]:
+      t1, b1 = div(s)
+      if b1 == b0:
+        t0 += t1
+        
+      else:
+        t0 = t0*b1 + t1*b0
+        b0 *= b1
+        # print('post', s, b0, t0)
+        
+      if t0 == 0:
+        b0 = 1
+
+      else:
+        g = gcd(t0, b0)
+        t0 //= g
+        b0 //= g
+        
+      # print(s, (t1, b1), (t0, b0))
+    
+    return f'{t0}/{b0}'
+  

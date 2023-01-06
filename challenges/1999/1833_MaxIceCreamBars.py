b4db0e880a2ff7f12d89d7b3a224ab1d452c9db7
Chopinsky <chopinsky@live.com>
2023-01-05 19:10:01 -0600

    1833,2244

--- challenges/1999/1833_MaxIceCreamBars.py
+++ challenges/1999/1833_MaxIceCreamBars.py
@@ -37,6 +37,17 @@ from typing import List
 
 class Solution:
   def maxIceCream(self, costs: List[int], coins: int) -> int:
+    costs.sort()
+    idx = 0
+    
+    while idx < len(costs) and coins >= costs[idx]:
+      coins -= costs[idx]
+      idx += 1
+    
+    return idx
+
+    
+  def maxIceCream(self, costs: List[int], coins: int) -> int:
     costs.sort(reverse=True)
     cnt = 0
     

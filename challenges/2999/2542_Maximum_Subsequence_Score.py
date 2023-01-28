65e3b8cb15c9e2736014d9d16b31b85bd10ffd84
Chopinsky <chopinsky@live.com>
2023-01-27 22:11:40 -0600

    352,2542

---
+++ challenges/2999/2542_Maximum_Subsequence_Score.py
@@ -0,0 +1,97 @@
+'''
+2542. Maximum Subsequence Score
+
+You are given two 0-indexed integer arrays nums1 and nums2 of equal length n and a positive integer k. You must choose a subsequence of indices from nums1 of length k.
+
+For chosen indices i0, i1, ..., ik - 1, your score is defined as:
+
+The sum of the selected elements from nums1 multiplied with the minimum of the selected elements from nums2.
+It can defined simply as: (nums1[i0] + nums1[i1] +...+ nums1[ik - 1]) * min(nums2[i0] , nums2[i1], ... ,nums2[ik - 1]).
+Return the maximum possible score.
+
+A subsequence of indices of an array is a set that can be derived from the set {0, 1, ..., n-1} by deleting some or no elements.
+
+Example 1:
+
+Input: nums1 = [1,3,3,2], nums2 = [2,1,3,4], k = 3
+Output: 12
+Explanation: 
+The four possible subsequence scores are:
+- We choose the indices 0, 1, and 2 with score = (1+3+3) * min(2,1,3) = 7.
+- We choose the indices 0, 1, and 3 with score = (1+3+2) * min(2,1,4) = 6. 
+- We choose the indices 0, 2, and 3 with score = (1+3+2) * min(2,3,4) = 12. 
+- We choose the indices 1, 2, and 3 with score = (3+3+2) * min(1,3,4) = 8.
+Therefore, we return the max score, which is 12.
+Example 2:
+
+Input: nums1 = [4,2,3,1,1], nums2 = [7,5,10,9,6], k = 1
+Output: 30
+Explanation: 
+Choosing index 2 is optimal: nums1[2] * nums2[2] = 3 * 10 = 30 is the maximum possible score.
+
+Constraints:
+
+n == nums1.length == nums2.length
+1 <= n <= 10^5
+0 <= nums1[i], nums2[j] <= 10^5
+1 <= k <= n
+'''
+
+from typing import List
+
+
+class Solution:
+  def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+    if k == 1:
+      return max(v0*v1 for v0, v1 in zip(nums1, nums2))
+    
+    n = len(nums1)
+    if k == n:
+      return sum(nums1) * min(nums2)
+    
+    floor = sorted((val, idx) for idx, val in enumerate(nums2))
+    src = sorted((val, idx) for idx, val in enumerate(nums1))
+    
+    # print(floor, src)
+    sums = nums1[floor[0][1]]
+    used = set([floor[0][1]])
+    
+    while len(used) < k:
+      val, idx = src.pop()
+      if idx in used:
+        continue
+        
+      sums += val 
+      used.add(idx)
+    
+    score = sums * floor[0][0]
+    # print(score, sums, floor[0][0], used)
+    
+    for i in range(1, n):
+      _, j0 = floor[i-1]
+      sums -= nums1[j0]
+
+      floor_val, j1 = floor[i]
+      if j1 in used:
+        # the new floor index already in the subseq, add
+        # another large number's index instead
+        while src and src[-1][1] in used:
+          src.pop()
+      
+        if not src:
+          break
+          
+        val_add, j1 = src.pop()
+        
+      else:
+        # add the floor index to the subseq
+        val_add = nums1[j1]
+
+      sums += val_add
+      used.add(j1)
+        
+      # print('pop:', nums1[j], sums*v2)
+      score = max(score, sums * floor_val)
+      
+    return score
+    
--- challenges/499/352.DataStreamJointIntervals.py
+++ challenges/499/352.DataStreamJointIntervals.py
@@ -39,7 +39,75 @@ At most 3 * 104 calls will be made to addNum and getIntervals.
 Follow up: What if there are lots of merges and the number of disjoint intervals is small compared to the size of the data stream?
 '''
 
+from heapq import heappop, heappush
+from typing import List
+from bisect import bisect_left
+
+
 class SummaryRanges:
+  def __init__(self):
+    self.arr = []
+    self.stack = []
+    self.seen = set()
+
+
+  def addNum(self, value: int) -> None:
+    if value not in self.seen:
+      heappush(self.stack, value)
+      self.seen.add(value)
+
+      
+  def nxt_range_to_merge(self):
+    left = heappop(self.stack)
+    right = left
+    
+    while self.stack and right+1 == self.stack[0]:
+      right = heappop(self.stack)
+      
+    return (left, right)
+      
+      
+  def getIntervals(self) -> List[List[int]]:
+    if not self.stack:
+      return self.arr
+    
+    nxt_arr = []
+    idx, n = 0, len(self.arr)
+    # print(self.stack, self.arr, n)
+    
+    while self.stack:
+      # get the range from the next numbers batch from the data
+      # stream since the last query
+      l, r = self.nxt_range_to_merge()
+      
+      # merge this range with the last extended range if overlapping
+      if nxt_arr and nxt_arr[-1][1] >= l-1:
+        last_left, last_right = nxt_arr.pop()
+        l = min(l, last_left)
+        r = max(r, last_right)
+      
+      # adding all ranges to the left of the current range in action
+      while idx < n and self.arr[idx][1] < l-1:
+        nxt_arr.append(self.arr[idx])
+        idx += 1
+        
+      # merging all overlapping ranges
+      while idx < n and self.arr[idx][0] <= r+1:
+        l = min(l, self.arr[idx][0])
+        r = max(r, self.arr[idx][1])
+        idx += 1
+        
+      nxt_arr.append((l, r))
+      
+    if idx < n:
+      nxt_arr += self.arr[idx:]
+      
+    self.arr = nxt_arr
+    
+    return nxt_arr
+
+
+class SummaryRanges0:
   def __init__(self):
     self.store = []
 

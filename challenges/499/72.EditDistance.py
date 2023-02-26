f063c4064b99d1111a55ef4f3d059d22f15b1972
Chopinsky <chopinsky@live.com>
2023-02-25 20:13:33 -0600

    72,1792

--- challenges/499/72.EditDistance.py
+++ challenges/499/72.EditDistance.py
@@ -34,7 +34,35 @@ Constraints:
 word1 and word2 consist of lowercase English letters.
 '''
 
+from functools import lru_cache
+
+
 class Solution:
+  def minDistance(self, word1: str, word2: str) -> int:
+    n1, n2 = len(word1), len(word2)
+    
+    @lru_cache(None)
+    def dp(i: int, j: int) -> int:
+      if i >= n1:
+        return n2-j
+      
+      if j >= n2:
+        return n1-i
+      
+      if word1[i] == word2[j]:
+        return dp(i+1, j+1)
+      
+      ops = 1 + min(
+        dp(i+1, j+1), # replace
+        dp(i+1, j),   # delete/insert
+        dp(i, j+1),   # insert/delete
+      )
+      
+      return ops
+    
+    return dp(0, 0)
+    
+
   # bottom up dp
   def minDistance(self, word1: str, word2: str) -> int:
     l1, l2 = len(word1), len(word2)
@@ -59,6 +87,7 @@ class Solution:
         )
     
     return dp[-1][-1]
+
 
   # top down dp
   def minDistance0(self, word1: str, word2: str) -> int:

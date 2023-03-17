facca374dbdde8b670f2261cbf6e501129511dfe
Chopinsky <chopinsky@live.com>
2023-03-16 19:35:06 -0500

    208,2336

--- challenges/499/208.ImplTrie.py
+++ challenges/499/208.ImplTrie.py
@@ -35,6 +35,47 @@ At most 3 * 104 calls in total will be made to insert, search, and startsWith.
 
 class Trie:
   def __init__(self):
+    self.root = {}
+
+
+  def insert(self, word: str) -> None:
+    curr = self.root
+    
+    for ch in word:
+      if ch not in curr:
+        curr[ch] = {}
+        
+      curr = curr[ch]
+      
+    curr['$'] = None
+
+
+  def search(self, word: str) -> bool:
+    curr = self.root
+    
+    for ch in word:
+      if ch not in curr:
+        return False
+      
+      curr = curr[ch]
+      
+    return '$' in curr
+
+
+  def startsWith(self, prefix: str) -> bool:
+    curr = self.root
+    
+    for ch in prefix:
+      if ch not in curr:
+        return False
+      
+      curr = curr[ch]
+      
+    return len(curr) > 0
+        
+
+class Trie:
+  def __init__(self):
     self.is_word = False
     self.children = [None] * 26
 

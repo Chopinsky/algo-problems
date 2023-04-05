e039e0e6596172ea2e6adfb8d8412b5c4ee4e2c7
Chopinsky <chopinsky@live.com>
2023-04-04 20:41:37 -0500

    2439,2607

--- challenges/2499/2439_MinimizeMaximumArray.py
+++ challenges/2499/2439_MinimizeMaximumArray.py
@@ -41,6 +41,31 @@ from typing import List
 
 class Solution:
   def minimizeArrayValue(self, nums: List[int]) -> int:
+    l, r = min(nums), max(nums)
+    n = len(nums)
+    max_val = r
+    
+    def check(val):
+      overflow = 0
+      for i in range(n-1, -1, -1):
+        diff = nums[i] - val
+        overflow = max(0, overflow+diff)
+                    
+      return overflow == 0
+    
+    while l <= r:
+      mid = (l + r) // 2
+      
+      if check(mid):
+        max_val = min(max_val, mid)
+        r = mid - 1
+      else:
+        l = mid + 1
+      
+    return max_val
+  
+  
+  def minimizeArrayValue(self, nums: List[int]) -> int:
       stack = []
       n, idx = len(nums), 0
       

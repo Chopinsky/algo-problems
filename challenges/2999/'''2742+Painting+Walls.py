c64cba3065551871fd4d164346290703dfbb3a55
Chopinsky <chopinsky@live.com>
2023-06-18 11:36:27 -0500

    2742

---
+++ challenges/2999/'''2742+Painting+Walls.py
@@ -0,0 +1,54 @@
+'''
+2742. Painting the Walls
+
+You are given two 0-indexed integer arrays, cost and time, of size n representing the costs and the time taken to paint n different walls respectively. There are two painters available:
+
+A paid painter that paints the ith wall in time[i] units of time and takes cost[i] units of money.
+A free painter that paints any wall in 1 unit of time at a cost of 0. But the free painter can only be used if the paid painter is already occupied.
+Return the minimum amount of money required to paint the n walls.
+
+Example 1:
+
+Input: cost = [1,2,3,2], time = [1,2,3,2]
+Output: 3
+Explanation: The walls at index 0 and 1 will be painted by the paid painter, and it will take 3 units of time; meanwhile, the free painter will paint the walls at index 2 and 3, free of cost in 2 units of time. Thus, the total cost is 1 + 2 = 3.
+Example 2:
+
+Input: cost = [2,3,4,2], time = [1,1,1,1]
+Output: 4
+Explanation: The walls at index 0 and 3 will be painted by the paid painter, and it will take 2 units of time; meanwhile, the free painter will paint the walls at index 1 and 2, free of cost in 2 units of time. Thus, the total cost is 2 + 2 = 4.
+
+Constraints:
+
+1 <= cost.length <= 500
+cost.length == time.length
+1 <= cost[i] <= 10^6
+1 <= time[i] <= 500
+'''
+
+from typing import List
+import math
+
+
+class Solution:
+  '''
+  the idea is that `time` is also an translatable value to `cost`: for job-i, a paid labor will
+  make it possible to finish `time[i]+1` walls, as the paid labor finish wall-i, then free
+  labor finish `time[i]` walls. 
+
+  Then the question becomes `given cnt walls, and the paid labor has done "cost", "time" to paint
+  a wall, what's the lowest cost? It will be the cost to paint `cnt-t-1` number of walls plus the
+  "cost".
+  '''
+  def paintWalls(self, cost: List[int], time: List[int]) -> int:
+    n = len(cost)
+    dp = [0] + [math.inf] * n
+    
+    for c, t in zip(cost, time):
+      for cnt in range(n, 0, -1):
+        # use paid worker to finish 1 wall and free worker to
+        # finish t walls
+        dp[cnt] = min(dp[cnt], dp[max(0, cnt-t-1)]+c)
+      
+    return dp[n]
+        

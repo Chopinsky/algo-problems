fbaead5533e584ba0914fd591cbc59c3920efb5f
Chopinsky <chopinsky@live.com>
2023-09-25 19:59:07 -0500

    2712

--- challenges/499/316+Remove+Duplicate+Letters.py
+++ challenges/499/316+Remove+Duplicate+Letters.py
@@ -19,6 +19,32 @@ s consists of lowercase English letters.
 
 
 class Solution:
+  def removeDuplicateLetters(self, s: str) -> str:
+    stack = []
+    last = {}
+    n = len(s)
+    
+    for i, ch in enumerate(s):
+      last[ch] = i
+      
+    # print(last)
+    seen = set()
+    
+    for i, c0 in enumerate(s):
+      if c0 in seen:
+        continue
+        
+      while stack and stack[-1] > c0 and last[stack[-1]] > i:
+        c1 = stack.pop()
+        seen.discard(c1)
+      
+      stack.append(c0)
+      seen.add(c0)
+      # print('iter:', i, c0, stack)
+    
+    return ''.join(stack)
+        
+        
   '''
   the idea is to pop the tail char that is larger than the current char, and the
   tail char can be added back in later part of the string

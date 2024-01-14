bca0cebd185baa2b11ad37a9e975954a1fd154e9
Chopinsky <chopinsky@live.com>
2024-01-13 22:57:45 -0600

    3008

---
+++ challenges/3499/3008+Find+Beautiful+Indices+Given+Array+II.py
@@ -0,0 +1,123 @@
+'''
+3008. Find Beautiful Indices in the Given Array II
+
+You are given a 0-indexed string s, a string a, a string b, and an integer k.
+
+An index i is beautiful if:
+
+0 <= i <= s.length - a.length
+s[i..(i + a.length - 1)] == a
+There exists an index j such that:
+0 <= j <= s.length - b.length
+s[j..(j + b.length - 1)] == b
+|j - i| <= k
+Return the array that contains beautiful indices in sorted order from smallest to largest.
+
+Example 1:
+
+Input: s = "isawsquirrelnearmysquirrelhouseohmy", a = "my", b = "squirrel", k = 15
+Output: [16,33]
+Explanation: There are 2 beautiful indices: [16,33].
+- The index 16 is beautiful as s[16..17] == "my" and there exists an index 4 with s[4..11] == "squirrel" and |16 - 4| <= 15.
+- The index 33 is beautiful as s[33..34] == "my" and there exists an index 18 with s[18..25] == "squirrel" and |33 - 18| <= 15.
+Thus we return [16,33] as the result.
+Example 2:
+
+Input: s = "abcd", a = "a", b = "a", k = 4
+Output: [0]
+Explanation: There is 1 beautiful index: [0].
+- The index 0 is beautiful as s[0..0] == "a" and there exists an index 0 with s[0..0] == "a" and |0 - 0| <= 4.
+Thus we return [0] as the result.
+
+Constraints:
+
+1 <= k <= s.length <= 5 * 10^5
+1 <= a.length, b.length <= 5 * 10^5
+s, a, and b contain only lowercase English letters.
+'''
+
+from typing import List
+from bisect import bisect_left
+
+
+class Solution:
+  def beautifulIndices(self, s: str, a: str, b: str, k: int) -> List[int]:
+    mod = 10**9 + 7
+    p = 31
+    powers = [1]
+    ls = len(s)
+    hash_src = [0]
+    oa = ord('a')
+    
+    def calc(s, i):
+      return (ord(s[i]) - oa + 1) * powers[i]
+    
+    # build power bases
+    for i in range(1, ls):
+      nxt_power = (powers[-1] * p) % mod
+      powers.append(nxt_power)
+    
+    # build hash for s
+    for i in range(ls):
+      nxt_hash = (hash_src[-1] + calc(s, i)) % mod
+      hash_src.append(nxt_hash)
+    
+    # print('init:', powers, hash_src)
+    
+    def rabin_karp(src, ptn):
+      if len(ptn) > ls:
+        return []
+      
+      if len(ptn) == ls:
+        return [0] if src == ptn else []
+
+      ans = []
+      lp = len(ptn)
+      hash_ptn = 0
+      
+      for i in range(lp):
+        hash_ptn = (hash_ptn + calc(ptn, i)) % mod
+      
+      for i in range(ls):
+        if i+lp > ls:
+          break
+          
+        curr_hash = (hash_src[i+lp] + mod - hash_src[i]) % mod
+        pad_hash = (hash_ptn * powers[i]) % mod
+        if curr_hash == pad_hash:
+          ans.append(i)
+          # print(i, s[i:i+lp])
+          
+        # print('search:', curr_hash, pad_hash)
+        
+      # print('find:', ptn, ans)
+      
+      return sorted(ans)
+    
+    def check(arr, idx):
+      if not arr:
+        return False
+      
+      i = bisect_left(arr, idx)
+      if i >= len(arr):
+        return abs(idx - arr[-1]) <= k
+      
+      # print(idx, (i, arr[i]))
+      if abs(idx - arr[i]) <= k:
+        return True
+      
+      return i-1 >= 0 and abs(idx - arr[i-1]) <= k
+      
+    pa = rabin_karp(s, a)
+    pb = rabin_karp(s, b)
+    res = set()
+    
+    if not pa or not pb:
+      return list(res)
+    
+    for i in pa:
+      if check(pb, i):
+        res.add(i)
+        
+    return sorted(res)
+        

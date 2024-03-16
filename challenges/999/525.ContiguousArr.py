6ff6c586ffc5b2e69f518d69798cc66654b9c151
Chopinsky <chopinsky@live.com>
2024-03-15 22:16:46 -0500

    525

--- challenges/999/525.ContiguousArr.py
+++ challenges/999/525.ContiguousArr.py
@@ -19,11 +19,23 @@ Constraints:
 nums[i] is either 0 or 1.
 '''
 
-
 from typing import List
 
-
 class Solution:
+  def findMaxLength(self, nums: List[int]) -> int:
+    pos = {0:-1}
+    curr = 0
+    ln = 0
+    
+    for i, val in enumerate(nums):
+      curr += 1 if val == 1 else -1
+      if curr not in pos:
+        pos[curr] = i
+      else:
+        ln = max(ln, i-pos[curr])
+        
+    return ln
+        
   def findMaxLength(self, nums: List[int]) -> int:
     if 0 not in nums or 1 not in nums:
       return 0

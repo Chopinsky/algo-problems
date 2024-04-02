2a03fa7ffebd1feacfa28de1836418b216f65535
Chopinsky <chopinsky@live.com>
2024-04-01 19:25:18 -0500

    3075

--- challenges/499/205.IsomorphicStrings.py
+++ challenges/499/205.IsomorphicStrings.py
@@ -22,13 +22,35 @@ Output: true
 
 Constraints:
 
-1 <= s.length <= 5 * 10 ** 4
+1 <= s.length <= 5 * 10^4
 t.length == s.length
 s and t consist of any valid ascii character.
 '''
 
-
 class Solution:
+  def isIsomorphic(self, s: str, t: str) -> bool:
+    if len(s) != len(t):
+      return False
+    
+    change = {}
+    used = set()
+    
+    for i in range(len(s)):
+      cs = s[i]
+      ct = t[i]
+      
+      if cs not in change:
+        if ct in used:
+          return False
+        
+        change[cs] = ct
+        used.add(ct)
+        
+      if ct != change[cs]:
+        return False
+    
+    return True
+        
   def isIsomorphic(self, s: str, t: str) -> bool:
     ls, lt = len(s), len(t)
     if ls != lt:

badbc1e3f76f49710e1d6b6cc9f622be6f8322bf
Chopinsky <chopinsky@live.com>
2024-04-13 12:33:36 -0500

    3113

--- challenges/499/85.MaxRect.py
+++ challenges/499/85.MaxRect.py
@@ -32,11 +32,43 @@ cols == matrix[i].length
 matrix[i][j] is '0' or '1'.
 '''
 
-
 from typing import List
 
-
-class Solution(object):
+class Solution:
+  def maximalRectangle(self, mat: List[List[str]]) -> int:
+    m, n = len(mat), len(mat[0])
+    prefix = [[int(val) for val in mat[i]] for i in range(m)]
+    area = 0
+    
+    for x in range(m):
+      for y in range(1, n):
+        if mat[x][y] == '0':
+          continue
+          
+        prefix[x][y] += prefix[x][y-1]
+        
+      # print(prefix[x])
+      
+    for x0 in range(m):
+      for y in range(n):
+        if prefix[x0][y] == 0:
+          continue
+          
+        if (x0+1)*(y+1) <= area:
+          continue
+          
+        width = prefix[x0][y]
+        area = max(area, width)
+        
+        for x1 in range(x0, -1, -1):
+          if prefix[x1][y] == 0:
+            break
+            
+          width = min(width, prefix[x1][y])
+          area = max(area, (x0-x1+1)*width)
+          
+    return area
+        
   def maximalRectangle(self, matrix: List[List[int]]) -> int:
     if not matrix:
       return 0

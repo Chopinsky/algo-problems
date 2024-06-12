4414da78a89110c71aab2ae0c7f03faa19c665bb
Chopinsky <chopinsky@live.com>
2024-06-11 19:24:08 -0500

    1608

--- challenges/499/75.SortColors.py
+++ challenges/499/75.SortColors.py
@@ -41,6 +41,27 @@ class Solution:
     """
     Do not return anything, modify nums in-place instead.
     """
+    r, w, b = 0, 0, 0
+    for c in nums:
+      if c == 0:
+        r += 1
+      elif c == 1:
+        w += 1
+      else:
+        b += 1
+        
+    for i in range(len(nums)):
+      if i < r:
+        nums[i] = 0
+      elif i < r+w:
+        nums[i] = 1
+      else:
+        nums[i] = 2
+        
+  def sortColors(self, nums: List[int]) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
 
     n = len(nums)
     l, r = 0, n-1

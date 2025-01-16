495fc4c0aae7861059f7049fbbcda409bda4d3a9
Jacob Zuo <jacob.dzwo@gmail.com>
2025-01-15 18:03:07 -0800

    2425

--- challenges/2499/2425_BitwiseXORAllPairings.py
+++ challenges/2499/2425_BitwiseXORAllPairings.py
@@ -7,6 +7,23 @@ from typing import List
 
 class Solution:
   def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
+    n1, n2 = len(nums1), len(nums2)
+    v1, v2 = 0, 0
+
+    for val in nums1:
+      v1 ^= val
+
+    for val in nums2:
+      v2 ^= val
+
+    xor1 = v1 if n2%2 == 1 else 0
+    xor2 = v2 if n1%2 == 1 else 0
+    # print('init:', v1, v2)
+    # print('xor:', xor1, xor2)
+
+    return xor1^xor2
+
+  def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
     base1 = nums1[0] 
     for val in nums1[1:]:
       base1 ^= val

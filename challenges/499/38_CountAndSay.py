1b1620b9e8bc600c8f04bd021ae1d77d165d2302
Jacob Zuo <jacob.dzwo@gmail.com>
2025-04-17 19:26:33 -0700

    38

--- challenges/499/38_CountAndSay.py
+++ challenges/499/38_CountAndSay.py
@@ -2,6 +2,34 @@ from functools import lru_cache
 
 
 class Solution:
+  def countAndSay(self, n: int) -> str:
+    def dp(n):
+      if n == 1:
+        return "1"
+
+      prev = dp(n-1)
+      res = ""
+      val = ""
+      cnt = 0
+
+      for i in range(len(prev)):
+        d = prev[i]
+        if d == val:
+          cnt += 1
+        else:
+          if val:
+            res += str(cnt) + val
+
+          val = d
+          cnt = 1
+
+      if val:
+        res += str(cnt) + val
+        
+      return res
+
+    return dp(n)
+        
   @lru_cache(None)
   def say(val: str) -> str:
     res = ''
